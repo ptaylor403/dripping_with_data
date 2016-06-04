48bfc2a761dc6f5a96dc4a2d145429abf82f68ed
Trisha <ptaylor403@gmail.com>
2016-06-04 16:13:23 -0400

    started on testing

--- hpv/tests.py
+++ hpv/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Attendance, Complete
+import datetime
 # Create your tests here.
+class AttendanceTestCase(TestCase):
+    def setUP(self):
+        employee1 = Attendance.objects.create(employee_number=1,
+                                  department='FCB',
+                                  clock_in_time=datetime.datetime.now(),
+                                  clock_out_time=Null,
+                                  shift='shift')
+        employee2 = Attendance.objects.create(employee_number=2,
+                                  department='CIW',
+                                  clock_in_time=datetime.datetime.now(),
+                                  clock_out_time=Null,
+                                  shift='shift')
+
+
+    def test_clocked_in(self):
+        pass
