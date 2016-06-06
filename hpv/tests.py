27ef05e4fa3a3d678c71c2b235611cdede771e5b
CameronB <cboudreau6238@gmail.com>
2016-06-05 23:33:44 -0400

    first test for hpv model method

--- hpv/tests.py
+++ hpv/tests.py
@@ -1,15 +1,43 @@
 from django.test import TestCase
 from .models import Attendance
-from datetime import datetime
+import datetime as dt
 
 
 class AttendanceTestCase(TestCase):
+
     def setUp(self):
         #first shift clocked out
-        Attendance.objects.create(employee_number=00001, department='CIW', clock_in_time=datetime(2016, 6, 1, 6, 30), clock_out_time=datetime(2016, 6, 1, 2, 30), shift='first')
+        Attendance.objects.create(employee_number=00001,
+                                  department='CIW',
+                                  clock_in_time=datetime(2016, 6, 1, 6, 30),
+                                  clock_out_time=datetime(2016, 6, 1, 2, 30),
+                                  shift='first')
+
         #first shift not clocked out
-        Attendance.objects.create(employee_number=00002, department='CIW', clock_in_time=datetime(2016, 6, 1, 6, 30), clock_out_time=None, shift='first')
+        Attendance.objects.create(employee_number=00002,
+                                  department='FCH',
+                                  clock_in_time=datetime(2016, 6, 1, 6, 30),
+                                  clock_out_time=None,
+                                  shift='first')
+
         #second shift clocked out
-        Attendance.objects.create(employee_number=00003, department='CIW', clock_in_time=datetime(2016, 6, 1, 2, 30), clock_out_time=datetime(2016, 6, 1, 11, 0), shift='first')
+        Attendance.objects.create(employee_number=00003,
+                                  department='FCB',
+                                  clock_in_time=datetime(2016, 6, 1, 14, 30),
+                                  clock_out_time=datetime(2016, 6, 1, 11, 0),
+                                  shift='second')
+
         #second shift not clocked out
-        Attendance.objects.create(employee_number=00004, department='CIW', clock_in_time=datetime(2016, 6, 1, 6, 30), clock_out_time=None, shift='first')
+        Attendance.objects.create(employee_number=00004,
+                                  department='PNT',
+                                  clock_in_time=datetime(2016, 6, 1, 14, 30),
+                                  clock_out_time=None,
+                                  shift='second')
+
+    def test_get_manhours_during_first_shift_plus_1_hour(self):
+        start = dt.datetime(2016, 6, 1, 6, 30)
+        stop = dt.datetime(2016, 6, 1, 15, 30)
+        expected_manhours = 19
+
+        manhours = Attendance.get_manhours_during(start, stop)
+        self.assertEqual(manhours, expected_manhours)
