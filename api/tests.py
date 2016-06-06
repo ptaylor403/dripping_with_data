b98a5f7380b39a7e542fe2e0606f9fa445a88583
CameronB <cboudreau6238@gmail.com>
2016-06-06 00:56:49 -0400

    api tests pass 200 response

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,23 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
+from .models import HPVATM
+from .views import HPVAPI
+import pytz
+import datetime as dt
 
+class HPVAPITest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = APIRequestFactory()
 
-# Create your tests here.
+        # Create 2 test objects
+        HPVATM.objects.create(timestamp=pytz.utc.localize(dt.datetime(2016, 6, 1, 10, 30)), hpv_plant=89.9)
+
+        HPVATM.objects.create(timestamp=pytz.utc.localize(dt.datetime(2016, 6, 1, 15, 30)), hpv_plant=86.7)
+
+    def test_view(self):
+        request = self.factory.get('/api/hpv')
+
+        response = HPVAPI.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual()
--- hpv/tests.py
+++ hpv/tests.py
@@ -1,42 +1,43 @@
 from django.test import TestCase
 from .models import Attendance
 import datetime as dt
+import pytz
 
 
 class AttendanceTestCase(TestCase):
 
     def setUp(self):
         #first shift clocked out
-        Attendance.objects.create(employee_number=00001,
+        Attendance.objects.create(employee_number=66661,
                                   department='CIW',
-                                  clock_in_time=datetime(2016, 6, 1, 6, 30),
-                                  clock_out_time=datetime(2016, 6, 1, 2, 30),
+                                  clock_in_time=dt.datetime(2016, 6, 1, 6, 30),
+                                  clock_out_time=dt.datetime(2016, 6, 1, 14, 30),
                                   shift='first')
 
         #first shift not clocked out
-        Attendance.objects.create(employee_number=00002,
+        Attendance.objects.create(employee_number=66662,
                                   department='FCH',
-                                  clock_in_time=datetime(2016, 6, 1, 6, 30),
+                                  clock_in_time=dt.datetime(2016, 6, 1, 6, 30),
                                   clock_out_time=None,
                                   shift='first')
 
         #second shift clocked out
-        Attendance.objects.create(employee_number=00003,
+        Attendance.objects.create(employee_number=66663,
                                   department='FCB',
-                                  clock_in_time=datetime(2016, 6, 1, 14, 30),
-                                  clock_out_time=datetime(2016, 6, 1, 11, 0),
+                                  clock_in_time=dt.datetime(2016, 6, 1, 14, 30),
+                                  clock_out_time=dt.datetime(2016, 6, 1, 22, 30),
                                   shift='second')
 
         #second shift not clocked out
-        Attendance.objects.create(employee_number=00004,
+        Attendance.objects.create(employee_number=66664,
                                   department='PNT',
-                                  clock_in_time=datetime(2016, 6, 1, 14, 30),
+                                  clock_in_time=dt.datetime(2016, 6, 1, 14, 30),
                                   clock_out_time=None,
                                   shift='second')
 
     def test_get_manhours_during_first_shift_plus_1_hour(self):
-        start = dt.datetime(2016, 6, 1, 6, 30)
-        stop = dt.datetime(2016, 6, 1, 15, 30)
+        start = pytz.utc.localize(dt.datetime(2016, 6, 1, 6, 30))
+        stop = pytz.utc.localize(dt.datetime(2016, 6, 1, 15, 30))
         expected_manhours = 19
 
         manhours = Attendance.get_manhours_during(start, stop)
