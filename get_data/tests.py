47c193b937612307ab3078bf308f55f1f487639c
Nic Colley <nic.colley@live.com>
2016-06-06 12:37:37 -0400

    initial version of HPV completed. HPV calc resides in get_data and in  RawPlantActivity.get_hpv_at_slice(start, stop). Requires start, stop times to be datetime objects.

--- get_data/tests.py
+++ get_data/tests.py
@@ -1,13 +1,47 @@
 from django.test import TestCase
-from .models import RawClockData
+from .models import RawClockData, RawPlantActivity
 import datetime as dt
 
 
 class ManHoursTestCase(TestCase):
-
     def setUp(self):
+        #################################################
+        # Previous Day Clock In/Out
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='001',
+            full_nam='Cameron',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 2, 5, 35),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 2, 14, 32)
+        )
 
-        # clocked in before start, didn't clock out
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='002',
+            full_nam='Nic',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 2, 22, 29),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 6, 32)
+        )
+        #################################################
+        # Previous Day Forgot Clock Out
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='001',
+            full_nam='Cameron',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 2, 5, 35),
+            end_rsn_txt='&missedOut',
+            PNCHEVNT_OUT=None
+        )
+        #################################################
+        # Current Day Clock In
+        #################################################
         RawClockData.objects.create(
             PRSN_NBR_TXT='001',
             full_nam='Cameron',
@@ -15,21 +49,9 @@ class ManHoursTestCase(TestCase):
             start_rsn_txt='&newShift',
             PNCHEVNT_IN=dt.datetime(2016, 6, 3, 5, 35),
             end_rsn_txt='&missedOut',
-            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 2, 31),
+            PNCHEVNT_OUT=None
         )
 
-        # clocked in after start, clocked out
-        RawClockData.objects.create(
-            PRSN_NBR_TXT='002',
-            full_nam='Nic',
-            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
-            start_rsn_txt='&newShift',
-            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 35),
-            end_rsn_txt='&missedOut',
-            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 2, 31),
-        )
-
-        # clocked out from previous shift but with today's clockin time
         RawClockData.objects.create(
             PRSN_NBR_TXT='003',
             full_nam='Trisha',
@@ -37,32 +59,9 @@ class ManHoursTestCase(TestCase):
             start_rsn_txt='&newShift',
             PNCHEVNT_IN=dt.datetime(2016, 6, 3, 5, 25),
             end_rsn_txt='&missedOut',
-            PNCHEVNT_OUT=dt.datetime(2016, 6, 2, 2, 59),
+            PNCHEVNT_OUT=None
         )
 
-        # clocked out early from current shift
-        RawClockData.objects.create(
-            PRSN_NBR_TXT='004',
-            full_nam='Stacey',
-            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
-            start_rsn_txt='&newShift',
-            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 25),
-            end_rsn_txt='&missedOut',
-            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 10, 31)
-        )
-
-        # clocked in right at start time
-        RawClockData.objects.create(
-            PRSN_NBR_TXT='005',
-            full_nam='Stacey',
-            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
-            start_rsn_txt='&newShift',
-            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 30),
-            end_rsn_txt='&missedOut',
-            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 2, 30)
-        )
-
-        # clocked in before start, didn't clock out
         RawClockData.objects.create(
             PRSN_NBR_TXT='006',
             full_nam='Alex',
@@ -73,57 +72,250 @@ class ManHoursTestCase(TestCase):
             PNCHEVNT_OUT=None
         )
 
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='004',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 25),
+            end_rsn_txt='missedOut',
+            PNCHEVNT_OUT=None,
+        )
 
-    def test_get_currently_clocked_in_before_start(self):
+        #################################################
+        # Employees late
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='005',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 30),
+            end_rsn_txt='&missedOut',
+            PNCHEVNT_OUT=None
+        )
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='002',
+            full_nam='Nic',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 35),
+            end_rsn_txt='&missedOut',
+            PNCHEVNT_OUT=None,
+        )
+
+        #################################################
+        # Clocked Out Early
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='004',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 25),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 10, 31)
+        )
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='002',
+            full_nam='Nic',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 35),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 8, 30),
+        )
+
+        #################################################
+        # Employee came back
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='004',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 12, 30),
+            end_rsn_txt='&missedOut',
+            PNCHEVNT_OUT=None
+        )
+
+        #################################################
+        # Clock out early
+        #################################################
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='001',
+            full_nam='Cameron',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 5, 35),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 14, 29),
+        )
+
+        #################################################
+        # Clock out regular
+        #################################################
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='006',
+            full_nam='Alex',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 19),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 14, 30),
+        )
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='005',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 6, 30),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 14, 30)
+        )
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='004',
+            full_nam='Stacey',
+            HM_LBRACCT_FULL_NAM='017.20000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 12, 30),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 14, 30)
+        )
+
+        RawClockData.objects.create(
+            PRSN_NBR_TXT='003',
+            full_nam='Trisha',
+            HM_LBRACCT_FULL_NAM='017.80000.00.80.07/1/-/017.P000080/-/-/-',
+            start_rsn_txt='&newShift',
+            PNCHEVNT_IN=dt.datetime(2016, 6, 3, 5, 25),
+            end_rsn_txt='&out',
+            PNCHEVNT_OUT=dt.datetime(2016, 6, 3, 14, 30)
+        )
+
+        #################################################
+        # Tests
+        #################################################
+
+    def test_get_currently_clocked_in(self):
         """Test for employees who clocked in before start is accurate"""
         start = dt.datetime(2016, 6, 3, 6, 30)
-        expected_employees = ['001', '004', '006']
+        expected_employees = ['001', '003', '004', '006', '005', '002']
+        func_name = 'test_get_currently_clocked_in_before_start'
 
-        # Should return 2 employees clocked in before start time
-        employees = RawClockData.get_clocked_in_before_start(start).order_by('pk')
-        self.assertEqual(employees.count(), 3)
+        employees = RawClockData.get_clocked_in(start)
+
+        # for employee in employees:
+        #     ManHoursTestCase.print_employee_results(func_name, employee)
+
+        self.assertEqual(employees.count(), 7)
 
         # Testing for accuracy of filter
         for employee, expected_employee_num in zip(employees, expected_employees):
-            self.assertEqual(employee.PRSN_NBR_TXT, expected_employee_num)
+            self.assertIn(employee.PRSN_NBR_TXT, expected_employees)
 
-    def test_get_clocked_in_after_start(self):
-        """Test for employees who clock in after start is accurate"""
+    def test_man_hours(self):
+        """Test for employees who clocked in before start is accurate"""
         start = dt.datetime(2016, 6, 3, 6, 30)
-        expected_employees = ['002', '005']
+        stop = dt.datetime(2016, 6, 3, 10, 30)
+        expected_employees = ['001', '003', '004', '006', '005', '002']
+        func_name = 'test_man_hours'
 
-        # Should return 2 employees clocked in after start time
-        employees = RawClockData.get_clocked_in_after_start(start).order_by('pk')
-        self.assertEqual(employees.count(), 2)
+        hours, num_employees = RawClockData.get_plant_man_hours_atm(start, stop)
 
-        for employee, expected_employee_num in zip(employees, expected_employees):
-            self.assertEqual(employee.PRSN_NBR_TXT, expected_employee_num)
+        self.assertEqual(hours, 28)
+        self.assertEqual(num_employees, 7)
 
-    def test_get_clocked_out_after_start(self):
-        """Test for employees who clocked out after start is accurate"""
+    @staticmethod
+    def print_employee_results(fun_name, objects):
+        print(
+            fun_name,
+            '---',
+            objects.PRSN_NBR_TXT,
+            ' ', objects.PNCHEVNT_IN,
+            ' ', objects.PNCHEVNT_OUT,
+            ' ', objects.end_rsn_txt
+        )
+
+
+class ClaimData(TestCase):
+    def setUp(self):
+        #################################################
+        # claim trucks previous day as test manhours
+        #################################################
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3849',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 2, 12, 25),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3850',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 2, 14, 25),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+        #################################################
+        # claim trucks same day as test manhours
+        #################################################
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3851',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 3, 6, 45),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3852',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 3, 6, 55),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3853',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 3, 7, 15),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+        RawPlantActivity.objects.create(
+            VEH_SER_NO='HZ3854',
+            POOL_TRIG_TYPE='VEH DLVRY',
+            TS_LOAD=dt.datetime(2016, 6, 3, 8, 15),
+            DATE_WORK=dt.datetime(2016, 6, 1, 5, 25),
+        )
+
+    def test_get_claim_data(self):
+        #regular test case of start to end time
         start = dt.datetime(2016, 6, 3, 6, 30)
-        expected_employees = ['002', '005']
+        num_trucks = RawPlantActivity.get_claims_date_range(start)
+        self.assertEqual(num_trucks, 4)
 
-        # Should return 2 employees clocked in after start time
-        employees = RawClockData.get_clocked_in_after_start(start).order_by('pk')
-        self.assertEqual(employees.count(), 2)
+        #testing range
+        start = dt.datetime(2016, 6, 2, 6, 30)
+        stop = dt.datetime(2016, 6, 3, 7, 30)
+        num_trucks = RawPlantActivity.get_claims_date_range(start, stop)
 
-        for employee, expected_employee_num in zip(employees, expected_employees):
-            self.assertEqual(employee.PRSN_NBR_TXT, expected_employee_num)
+        print("*"*50)
+        print(num_trucks)
 
-    def test_get_clocked_out_during_time_period(self):
-        start = dt.datetime(2016, 6, 3, 6, 30)
-        stop =  dt.datetime(2016, 6, 3, 2, 30)
-        expected_employees = ['006']
+        self.assertEqual(num_trucks, 5)
 
-        # Employees who clocked out after start
-        employees = RawClockData.get_clocked_in_after_start(start).order_by('pk')
-        for employee in employees:
-            print("CLOCKED OUT AFTER START EMP #", employee.PRSN_NBR_TXT)
+        claimed_objects = RawPlantActivity.get_claimed_objects_in_range(start,stop)
+        expected_claims = ['HZ3849', 'HZ3850', 'HZ3851', 'HZ3852', 'HZ3853']
+        not_expected_claims = ['HZ3854']
+
+        for claim in claimed_objects:
+            self.assertIn(claim.VEH_SER_NO, expected_claims)
+            self.assertNotIn(claim.VEH_SER_NO, not_expected_claims)
 
 
-        # Filtering that list to capture those who didn't clock out after stop
-        filtered_employees = RawClockData.get_clocked_out_during_time_period(stop, employees)
-        for filtered_employee in filtered_employees:
-            print("FILTERED EMP #", filtered_employee.PRSN_NBR_TXT)
 
