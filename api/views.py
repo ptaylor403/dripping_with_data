2f27d385bb7c018a7299d01df308277cadfe3d56
Stacy Zhao <stacyzhao@hotmail.com>
2016-06-09 11:23:00 -0400

    minor changes to heatmap

--- api/views.py
+++ api/views.py
@@ -11,18 +11,35 @@ class HPVAPI(LoginRequiredMixin, generics.ListCreateAPIView):
     serializer_class = HPVSerializer
     login_url = '/login/'
 
+
     # def get_queryset(self):
     #     queryset = HPVATM.objects.all()
-    #     date = self.request.query_params.get("date", None)
-    #     if date is not None:
-    #         date = int(date)
-    #         date = dt.datetime(2016, 6, date, 0, 1)
-    #         queryset = queryset.filter(timestamp__gte=date, timestamp__lt=date + dt.timedelta(days=1))
-    #
+    #     day = self.request.query_params.get("day", None)
+    #     week = self.request.query_params.get("startdate", "enddate")
+    #     # day = dt.datetime(2016, 6, 1, 0, 1) # change date to reflect current date
+    #     # enddate = startdate + dt.timedelta(days=7)
+    #     if day is not None:
+    #         day = int(day)
+    #         startdate = dt.datetime(2016, 6, 1, day, 1)
+    #         queryset = queryset.filter(timestamp__gte=startdate, timestamp__lt=startdate + dt.timedelta(days=1))
+    #     elif week is not None:
+    #         week = int(week)
+    #         startdate = dt.datetime(2016, 6, 1, week, 1)
+    #         enddate = startdate + dt.timedelta(days=7)
+    #         queryset = self.queryset.filter(timestamp__range=[startdate, enddate])
     #     return queryset
 
+    # def get_queryset(self):
+#         queryset = HPVATM.objects.all()
+#         date = self.request.query_params.get("date", None)
+#         if date is not None:
+#             date = int(date)
+#             date = dt.datetime(2016, 6, date, 0, 1)
+#             queryset = queryset.filter(timestamp__gte=date, timestamp__lt=date + dt.timedelta(days=1))
+#
+#         return queryset
     def get_queryset(self):
-        startdate = dt.datetime(2016, 6, 1, 0, 1) # change date to reflect current
+        startdate = dt.datetime(2016, 6, 1, 0, 1) # change date to reflect current date
         enddate = startdate + dt.timedelta(days=7)
-        queryset = HPVATM.objects.filter(timestamp__range=[startdate, enddate])
+        queryset = self.queryset.filter(timestamp__range=[startdate, enddate])
         return queryset
