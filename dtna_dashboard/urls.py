8d7ad744d64618919fccc746537ebe597a69ed7f
Stacy Zhao <stacyzhao@hotmail.com>
2016-06-14 22:35:03 -0400

    trying to merge more branches

--- dtna_dashboard/urls.py
+++ dtna_dashboard/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.apps import AppConfig
+from pytz import utc
+from apscheduler.schedulers.background import BackgroundScheduler
+import filelock
 
 
 urlpatterns = [
@@ -10,3 +14,29 @@ urlpatterns = [
     url(r'^getdata/', include('get_data.urls')),
     url(r'^dripper/', include('generic_dripper.urls'))
 ]
+
+
+class DataProcessorConfig(AppConfig):
+    name = 'data_processor'
+
+    def ready(self):
+        """
+        When django loads this will schedule an event that checks the server for new data to process every X minutes as defined in plant settings.
+        """
+        from data_processor.processor import get_new_hpv_data
+        from plantsettings.models import PlantSetting
+
+        print("-" * 50)
+        print('Entering get_new_hpv_data')
+
+        plant_settings = PlantSetting.objects.latest('timestamp')
+        timer = plant_settings.CHK_SRVR
+        lock = filelock.FileLock('locker.txt')
+        lock.timeout = 1
+        try:
+            lock.acquire()
+        except:
+            return
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(get_new_hpv_data, 'interval', seconds=2)
+        scheduler.start()
