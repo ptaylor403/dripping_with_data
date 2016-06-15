d6be3509bc533fbd2de09b6177e158e4e6034a70
Trisha <ptaylor403@gmail.com>
2016-06-15 11:15:21 -0400

    changed days on heatmap

--- hpv/static/hpv/detail.js
+++ hpv/static/hpv/detail.js
@@ -334,7 +334,7 @@ jQuery(function($) {
         legendElementWidth = gridSize * 2,
         buckets = 9,
         colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"],
-        days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+        days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
         times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12a"];
 
     var svg = d3.select("#heatmap").append("svg")
